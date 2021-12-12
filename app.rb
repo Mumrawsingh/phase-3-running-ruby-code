209ef9fa6bd3404f554daf3dea9131a5608587c8
Matthew Umrawsingh <matthew.umrawsingh@gmail.com>
2021-12-12 18:43:59 -0500

    commit tree 30e2e6b1c203223f27ba67ae11b5941f479e053a

---
+++ app.rb
@@ -0,0 +1,19 @@
+# app.rb
+puts "Hello World!"
+#puts "Hello world!"
+#puts "Hello world!"
+
+#print "Hello world!"
+#print "Hello world!"
+print "Pass this test, please."
+
+#puts [1, 2, 3]  
+# prints a vertical list due to puts auto line breaks.
+
+#print [1, 2, 3]
+#just prints array as displayed on line 13.
+
+p [1, 2, 3]
+#same as print, but it looks better somehow.
+
+#pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
